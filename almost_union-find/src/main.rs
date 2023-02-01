26b7b7d04f0df0178f1357695963b995f92988cc
Benjaneb <benjaneb1@gmail.com>
2023-02-01 15:51:11 +0100

    Too slow :(

--- almost_union-find/src/main.rs
+++ almost_union-find/src/main.rs
@@ -46,24 +46,40 @@ impl UnionFind {
         let q_root = self.find(q);
         // Set p's parent to the root of q
         self.parents[p] = q_root;
-
+        
+        // Add p as a child of the root of q
+        self.children[q_root].push(p);
+        
         // Set p's children's parent to the first child
-        for child in 0..self.children[p].len() {
-            let index = self.children[p][child];
-            self.parents[index] = self.children[p][0];
+        // and add the other children as children to the first child
+        for p_child in 0..self.children[p].len() {
+            let first_child = self.children[p][0];
+            
+            let child_index = self.children[p][p_child];
+            self.parents[child_index] = first_child;
+            eprintln!("Set {}'s parent to {}", child_index + 1, first_child + 1);
+            
+            if child_index != first_child {
+                self.children[first_child].push(child_index);
+                eprintln!("Add {} as child to {}", child_index + 1, first_child + 1);
+            }
         }
+
+        // Remove p's children
+        self.children[p].clear();
     }
     
     // Operation 3
-    // Prints the number of elements and the sum of elements in the set containing p
+    // Prints the number of child elements and their sum (including p)
     fn size_sum(&self, p: usize) -> (usize, usize) {
         // Return value and size of 1 if arrived at a leaf (end of tree)
         if self.children[p].len() == 0 {
             return (1, p + 1);
         }
 
-        let mut size = 0;
-        let mut sum = 0;
+        let mut size = 1;
+        let mut sum = p + 1; // Start with current root's value
+        eprintln!("Root: {sum}");
 
         // Recurse through every child and add it's size and sum
         for child in 0..self.children[p].len() {
@@ -71,6 +87,7 @@ impl UnionFind {
             let (child_size, child_sum) = self.size_sum(index);
             size += child_size;
             sum += child_sum;
+            eprintln!("sum += {child_sum}");
         }
 
         return (size, sum);
@@ -107,7 +124,9 @@ fn main() {
             1 => union_find.union(parameters[1] - 1, parameters[2] - 1),
             2 => union_find.move_number(parameters[1] - 1, parameters[2] - 1),
             3 => {
-                let (size, sum) = union_find.size_sum(parameters[1] - 1);
+                let root = union_find.find(parameters[1] - 1);
+                let (size, sum) = union_find.size_sum(root);
+                eprint!("Size, sum: ");
                 println!("{} {}", size, sum);
             },
             _ => (),
