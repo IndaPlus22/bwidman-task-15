4bf23be82298a18f67f6d43aaef22b4194e86322
Benjaneb <benjaneb1@gmail.com>
2023-02-03 11:44:48 +0100

    Revolt against the borrow checker✊

--- tree_structure/src/lib.rs
+++ tree_structure/src/lib.rs
@@ -1,35 +1,42 @@
-use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
-type ChildNode<T> = Option<Rc<Node<T>>>;
+type ChildNode<T> = Option<Arc<Mutex<Node<T>>>>;
 
-pub struct Node<T> where
-T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
+fn create_child<T>(key: T) -> ChildNode<T>
+where T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
+    Some(Arc::new(Mutex::new(Node { key, left: None, right: None })))
+}
+
+pub struct Node<T>
+where T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
     key: T,
     left: ChildNode<T>,
     right: ChildNode<T>,
 }
 
-pub struct AVLTree<T> where
-T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
+pub struct AVLTree<T>
+where T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
     root: ChildNode<T>
 }
 
-impl<T> AVLTree<T> where
-T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
+impl<T> AVLTree<T>
+where T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
     pub fn new() -> Self {
         AVLTree { root: None }
     }
 
     // Searches for node representing key.
     // Returns None if node does not exist.
-    pub fn search(&self, key: T) -> Option<&Rc<Node<T>>> {
-        let mut node = self.root.as_ref();
+    pub fn search(&self, key: T) -> ChildNode<T> {
+        let mut node = self.root.clone();
 
-        while node.is_some() && node.unwrap().key != key {
-            if key < node.unwrap().key {
-                node = node.unwrap().left.as_ref();
-            } else {
-                node = node.unwrap().right.as_ref();
+        while let Some(n) = node.clone() {
+            while n.lock().unwrap().key != key {
+                if key < n.lock().unwrap().key {
+                    node = n.lock().unwrap().left.clone();
+                } else {
+                    node = n.lock().unwrap().right.clone();
+                }
             }
         }
         return node;
@@ -37,26 +44,31 @@ T: Copy + Clone + PartialEq + PartialOrd + Eq + Ord {
 
     pub fn insert(&mut self, key: T) {
         let mut node = self.root.clone();
-        let mut parent: Option<Rc<Node<T>>> = None;
+        let mut parent: ChildNode<T> = None;
 
         // Find suitable empty spot in tree
-        while node.is_some() {
+        while let Some(n) = node.clone() {
             parent = node;
-            if key < node.unwrap().key {
-                node = &mut node.unwrap().left;
+            if key < n.lock().unwrap().key {
+                node = n.lock().unwrap().left.clone();
             } else {
-                node = &mut node.unwrap().right;
+                node = n.lock().unwrap().right.clone();
             }
         }
 
-        if parent.is_none() { // Empty tree
-            self.root = Some(Rc::new(Node { key, left: None, right: None }));
-        } else if key < parent.unwrap().key {
-            parent.unwrap().left = Some(Rc::new(Node { key, left: None, right: None }));
+        match parent {
+            None => self.root = create_child(key), // Empty tree
+            Some(p) => {
+                if key < p.lock().unwrap().key {
+                    p.lock().unwrap().left = create_child(key);
+                } else if key > p.lock().unwrap().key {
+                    p.lock().unwrap().right = create_child(key);
+                }
+            }
         }
     }
 
-    pub fn delete(&mut self, key: T) {
+    pub fn delete(&mut self, _key: T) {
         todo!()
     }
 }
@@ -67,6 +79,6 @@ mod tests {
 
     #[test]
     fn test1() {
-        let tree: AVLTree<u32> = AVLTree::new();
+        let _tree: AVLTree<u32> = AVLTree::new();
     }
 }
